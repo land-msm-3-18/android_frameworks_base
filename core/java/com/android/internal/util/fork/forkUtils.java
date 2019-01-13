83ea197ea542c54b6e12d5d7b03b00611a5a9ddc
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Utils: Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- core/java/com/android/internal/util/fork/forkUtils.java
+++ core/java/com/android/internal/util/fork/forkUtils.java
@@ -27,6 +27,7 @@ import android.content.om.OverlayInfo;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.SystemProperties;
+import android.util.DisplayMetrics;
 
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -140,4 +141,24 @@ public class forkUtils {
             return mService.getOverlayInfosForTarget(target, userId);
         }
     }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
+    }
+
 }
