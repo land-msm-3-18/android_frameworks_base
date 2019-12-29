277b63fca6d6507d53b4489f644f1b96cfbea815
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 18:05:15 +0530

    Add OnePlus Slate and SamsungOne font styles [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: Ib5fccc0d08c9987d5f1a8f9b31a8ca5c70f5c6db

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,8 @@ LOCAL_REQUIRED_MODULES := \
         FontRosemarySourceOverlay \
         FontSonySketchSourceOverlay \
         FontSurferSourceOverlay \
+	FontOneplusSlateSource \
+	FontSamsungOneSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
