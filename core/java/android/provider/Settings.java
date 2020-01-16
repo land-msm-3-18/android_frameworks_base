59f207fb7c3c12cda823acf8504310c10153b005
Ido Ben-Hur <idoybh2@gmail.com>
2020-01-16 18:34:45 +0530

    Allow hiding the lock icon on lockscreen [1/2]
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9409,6 +9409,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to hide lock icon on lockscreen
+         * {@hide}
+         */
+        public static final String HIDE_LOCKICON = "hide_lockicon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
