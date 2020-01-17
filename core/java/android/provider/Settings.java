d0e3b2df053834d69b14cba38e88cd0feb436c35
spezi77 <spezi7713@gmx.net>
2020-01-17 09:17:55 +0100

    fwb: cLock: User toggle to show/hide status area [1/2]
    Some custom clocks look cleaner w/o additional status line for weather information etc.
    So give the user the choice.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I4901d998ced8d53df0f0144c1d77ad8aa082cb65
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5084,6 +5084,12 @@ public final class Settings {
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
         /**
+         * Whether to display status area (date & weather information) on the cLock
+         * @hide
+         */
+        public static final String CLOCK_SHOW_STATUS_AREA = "clock_show_status_area";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
