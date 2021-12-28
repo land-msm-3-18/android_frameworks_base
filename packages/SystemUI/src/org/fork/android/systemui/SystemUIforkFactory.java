94903219f61d3916d321f219b27bdf8012e92f5d
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: If46fe528418daca5bcd7ce2cde221c9b185947d6

--- packages/SystemUI/src/org/fork/android/systemui/SystemUIforkFactory.java
+++ packages/SystemUI/src/org/fork/android/systemui/SystemUIforkFactory.java
@@ -1,18 +1,41 @@
 package org.fork.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
 
-import org.fork.android.systemui.dagger.DaggerGlobalRootComponentAEX;
-import org.fork.android.systemui.dagger.GlobalRootComponentAEX;
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
+
+import org.fork.android.systemui.dagger.DaggerGlobalRootComponentfork;
+import org.fork.android.systemui.dagger.GlobalRootComponentfork;
+import org.aospextended.android.systemui.dagger.SysUIComponentfork;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
 
-public class SystemUIAEXFactory extends SystemUIFactory {
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class SystemUIforkFactory extends SystemUIFactory {
     @Override
     protected GlobalRootComponent buildGlobalRootComponent(Context context) {
-        return DaggerGlobalRootComponentAEX.builder()
+        return DaggerGlobalRootComponentfork.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentfork) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
--- packages/SystemUI/src/org/fork/android/systemui/dagger/SysUIComponentfork.java
+++ packages/SystemUI/src/org/fork/android/systemui/dagger/SysUIComponentfork.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import org.aospextended.android.systemui.keyguard.KeyguardSliceProviderAEX;
+import org.aospextended.android.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface SysUIComponentfork extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         SysUIComponentfork build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(KeyguardSliceProviderAEX keyguardSliceProviderAEX);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
